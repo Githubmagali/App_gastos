e922107e7d0918c2d96bcdb75af88506253a9247
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-07-11 20:10:30 -0300

    cambios

--- public/bundle.js
+++ public/bundle.js
@@ -3212,12 +3212,34 @@ contenedorGastos.addEventListener('click', (e)=>{
 //editar gasto
 if(e.target.closest('[data-accion="editar-gasto"]')){
     //dataset.id obtiene el id del gasto
-gasto.dataset.id;
+const id = gasto.dataset.id;
 
 //Obtenemos los gastos guardados
 //JSON.parse para que los transforme en un objeto de javascript
 const gastosGuardados= JSON.parse(window.localStorage.getItem('gastosE'));
-console.log(gastosGuardados);
+
+
+//cada vez que el usuario presione el boton de editar aparezca las vaiables vacias para que pueda completar
+let precio =''; 
+let descripcion = '';
+
+//comprobamos si hay gastos guardados
+if(gastosGuardados && gastosGuardados.length > 0){
+    //identificamos cada elemento como gastoFor
+    gastosGuardados.forEach((gastoFor) => {
+        if(gastoFor.id === id){
+           precio= gastoFor.precio;
+           descripcion = gastoFor.descripcion;
+                }
+    });
+  //Le ponemos el id de la descripcion y el precio a los inputs del formulario
+  document.querySelector('#formulario-desc #descripcionInput').value = descripcion;
+  document.querySelector('#formulario-desc #precioInput').value = precio;
+  //para hacerle un atributo personalizado
+  document.querySelector('#formulario-desc').dataset.id = id;
+
+  abrirFormularioGasto();
+}
 }
 });
 
